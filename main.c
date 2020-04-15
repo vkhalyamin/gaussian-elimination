60c9bdb88eefa7bc22e5cb288fd4672318d6763d
Khalyamin Vladimir <vkhalyamin@protonmail.com>
2020-04-15 12:05:10 +0300

    chage input method

--- main.c
+++ main.c
@@ -4,21 +4,26 @@
 #include "io.h"
 #include "gauss.h"
 
+enum { EXIT = 48, CALCULATE }; /* key '0' code and key '1' code */
+
 void calculate();
 
 int main() 
 { 
-    int menu_item;
-    printf("GAUSSIAN ELIMINATION v3.1\nCopyright (C) 2018 Khalymain Vladimir\n\n");
-    while(1) {
-    	printf("0 - exit\n1 - calculate\n\nSelect menu item:");
-	scanf("%d", &menu_item);
+	int menu_item;
+	printf("GAUSSIAN ELIMINATION v3.1\nCopyright (C) 2018 Khalymain Vladimir\n\n");
+	while(1) {
+    		printf("0 - exit\n1 - calculate\n\nSelect menu item: ");
+		menu_item = getchar();
 
-	if(menu_item != 0 && menu_item != 1)
-		printf("\nNonexistent menu item! Please enter again.\n");
-	else
-		menu_item ? calculate() : exit(0);		
-    }  
+		switch(menu_item) {
+		case CALCULATE: calculate(); break;
+		case EXIT: exit(0);
+		default: printf("Incorrect menu item! Please enter again.\n");
+		}
+
+		menu_item = getchar();
+	}  
 }
 
 void calculate()
